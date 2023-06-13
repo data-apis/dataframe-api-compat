9ab2ca031135a7c2a39061e51fb4bb6c1fc72b12
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-06-13 14:12:29 +0100

    test insert

--- test_standard.py
+++ test_standard.py
@@ -305,11 +305,12 @@ def test_insert(library: str) -> None:
     pd.testing.assert_frame_equal(df_pd, expected)
 
 
-def test_drop_column() -> None:
-    df = PandasDataFrame(pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}))
-    result = df.drop_column("a").dataframe
+def test_drop_column(library: str) -> None:
+    df = integer_dataframe_1(library)
+    result = df.drop_column("a")
+    result_pd = pd.api.interchange.from_dataframe(result.dataframe)
     expected = pd.DataFrame({"b": [4, 5, 6]})
-    pd.testing.assert_frame_equal(result, expected)
+    pd.testing.assert_frame_equal(result_pd, expected)
 
 
 def test_drop_column_invalid() -> None:
