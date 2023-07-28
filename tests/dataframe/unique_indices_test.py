bb828b9e862137eccf2d80c2c995ff9ef88f5939
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-07-28 15:57:45 +0100

    wip

---
+++ tests/dataframe/unique_indices_test.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+from tests.utils import integer_dataframe_6, convert_dataframe_to_pandas_numpy
+import pandas as pd
+
+
+@pytest.mark.parametrize(
+    ("keys", "expected_data"),
+    [
+        (["a", "b"], {"a": [1, 1, 2, 2], "b": [3, 4, 1, 2]}),
+        (None, {"a": [1, 1, 2, 2], "b": [3, 4, 1, 2]}),
+        (["a"], {"a": [1, 2], "b": [4, 1]}),
+        (["b"], {"a": [2, 2, 1, 1], "b": [1, 2, 3, 4]}),
+    ],
+)
+def test_unique_indices(
+    library: str, keys: list[str] | None, expected_data: dict[str, list[int]]
+) -> None:
+    df = integer_dataframe_6(library)
+    df = df.get_rows(df.unique_indices(keys))
+    result = df.get_rows(df.sorted_indices(keys))
+    result_pd = pd.api.interchange.from_dataframe(result.dataframe)
+    result_pd = convert_dataframe_to_pandas_numpy(result_pd)
+    expected = pd.DataFrame(expected_data)
+    pd.testing.assert_frame_equal(result_pd, expected)
--- tests/test_standard.py
+++ tests/test_standard.py
@@ -8,7 +8,6 @@ import pandas as pd
 import polars as pl
 
 from tests.utils import (
-    integer_dataframe_6,
     null_dataframe_1,
     null_dataframe_2,
     nan_dataframe_1,
@@ -539,27 +538,6 @@ def test_column_len(library: str) -> None:
 def test_get_value(library: str) -> None:
     result = integer_series_1(library).get_value(0)
     assert result == 1
-
-
-@pytest.mark.parametrize(
-    ("keys", "expected_data"),
-    [
-        (["a", "b"], {"a": [1, 1, 2, 2], "b": [3, 4, 1, 2]}),
-        (None, {"a": [1, 1, 2, 2], "b": [3, 4, 1, 2]}),
-        (["a"], {"a": [1, 2], "b": [4, 1]}),
-        (["b"], {"a": [2, 2, 1, 1], "b": [1, 2, 3, 4]}),
-    ],
-)
-def test_unique_indices(
-    library: str, keys: list[str] | None, expected_data: dict[str, list[int]]
-) -> None:
-    df = integer_dataframe_6(library)
-    df = df.get_rows(df.unique_indices(keys))
-    result = df.get_rows(df.sorted_indices(keys))
-    result_pd = pd.api.interchange.from_dataframe(result.dataframe)
-    result_pd = convert_dataframe_to_pandas_numpy(result_pd)
-    expected = pd.DataFrame(expected_data)
-    pd.testing.assert_frame_equal(result_pd, expected)
 
 
 def test_unique_indices_column(library: str) -> None:
