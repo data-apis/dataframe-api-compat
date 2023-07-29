ee25c747481ede6030515414ab33c8d4b8f288c3
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-07-29 15:57:17 +0200

    rows by mask

--- tests/test_standard.py
+++ tests/test_standard.py
@@ -20,19 +20,6 @@ from tests.utils import (
 )
 
 
-def test_column_get_rows_by_mask(library: str) -> None:
-    ser = integer_series_1(library)
-    namespace = ser.__column_namespace__()
-    mask = namespace.column_from_sequence([True, False, True], dtype=namespace.Bool())
-    result = ser.get_rows_by_mask(mask)
-    result_pd = pd.api.interchange.from_dataframe(
-        namespace.dataframe_from_dict({"result": result}).dataframe
-    )["result"]
-    result_pd = convert_series_to_pandas_numpy(result_pd)
-    expected = pd.Series([1, 3], name="result")
-    pd.testing.assert_series_equal(result_pd, expected)
-
-
 def test_column_any(library: str) -> None:
     ser = bool_series_1(library)
     result = ser.any()
