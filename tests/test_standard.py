8ecd7950316d2d086cc06bd464f5a205ea072a92
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-07-29 09:17:19 +0100

    wip

--- tests/test_standard.py
+++ tests/test_standard.py
@@ -784,41 +784,6 @@ def test_column_names(library: str) -> None:
         namespace.dataframe_from_dict({"result": ser})
 
 
-@pytest.mark.parametrize(
-    "column_names",
-    [
-        ["a", "b"],
-        None,
-        ["a"],
-        ["b"],
-    ],
-)
-def test_fill_null(
-    library: str,
-    request: pytest.FixtureRequest,
-    column_names: list[str] | None,
-) -> None:
-    df = null_dataframe_2(library, request)
-    namespace = df.__dataframe_namespace__()
-    result = df.fill_null(0, column_names=column_names)
-    # todo: is there a way to test test this without if/then statements?
-    if column_names is None or column_names == ["a", "b"]:
-        if library == "polars":
-            assert result.dataframe["a"][2] == 0.0
-            assert result.dataframe["b"][2] == 0.0
-        else:
-            assert result.dataframe["a"][2] == 0.0
-            assert result.dataframe["b"][2] == 0.0
-    elif column_names == ["a"]:
-        assert result.dataframe["a"][2] == 0.0
-        assert namespace.is_null(result.dataframe["b"][2])
-    elif column_names == ["b"]:
-        assert namespace.is_null(result.dataframe["a"][2])
-        assert result.dataframe["b"][2] == 0.0
-    else:
-        raise AssertionError()
-
-
 def test_fill_null_column(
     library: str,
     request: pytest.FixtureRequest,
