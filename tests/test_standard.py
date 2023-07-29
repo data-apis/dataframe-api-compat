be1b4f2b53d10cbf36010731fac73c5cc580b9d1
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-07-29 12:22:13 +0200

    fill_Null

--- tests/test_standard.py
+++ tests/test_standard.py
@@ -11,7 +11,6 @@ from tests.utils import (
     bool_dataframe_1,
     bool_dataframe_2,
     bool_dataframe_3,
-    bool_dataframe_4,
     convert_series_to_pandas_numpy,
     convert_dataframe_to_pandas_numpy,
     convert_to_standard_compliant_column,
@@ -388,26 +387,6 @@ def test_all(library: str) -> None:
     result = df.all()
     result_pd = pd.api.interchange.from_dataframe(result.dataframe)
     expected = pd.DataFrame({"a": [False], "b": [False], "c": [True]})
-    pd.testing.assert_frame_equal(result_pd, expected)
-
-
-def test_or(library: str) -> None:
-    df = bool_dataframe_1(library)
-    other = bool_dataframe_4(library)
-    result = df | other
-    result_pd = pd.api.interchange.from_dataframe(result.dataframe)
-    result_pd = convert_dataframe_to_pandas_numpy(result_pd)
-    expected = pd.DataFrame({"a": [True, True, False], "b": [True, True, True]})
-    pd.testing.assert_frame_equal(result_pd, expected)
-
-
-def test_or_with_scalar(library: str) -> None:
-    df = bool_dataframe_1(library)
-    other = True
-    result = df | other
-    result_pd = pd.api.interchange.from_dataframe(result.dataframe)
-    result_pd = convert_dataframe_to_pandas_numpy(result_pd)
-    expected = pd.DataFrame({"a": [True, True, True], "b": [True, True, True]})
     pd.testing.assert_frame_equal(result_pd, expected)
 
 
