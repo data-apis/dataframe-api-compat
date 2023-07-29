909636e7221b14392039d005fc99dcc27f376a3a
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-07-29 12:17:34 +0200

    fill_Null

---
+++ tests/dataframe/fill_nan_test.py
@@ -0,0 +1,11 @@
+from tests.utils import nan_dataframe_1
+import pandas as pd
+
+
+def test_fill_nan(library: str) -> None:
+    df = nan_dataframe_1(library)
+    result = df.fill_nan(-1)
+    result_pd = pd.api.interchange.from_dataframe(result.dataframe)
+    result_pd = result_pd.astype("float64")
+    expected = pd.DataFrame({"a": [1.0, 2.0, -1.0]})
+    pd.testing.assert_frame_equal(result_pd, expected)
--- tests/test_standard.py
+++ tests/test_standard.py
@@ -660,15 +660,6 @@ def test_all_rowwise(library: str) -> None:
     pd.testing.assert_series_equal(result_pd, expected)
 
 
-def test_fill_nan(library: str) -> None:
-    df = nan_dataframe_1(library)
-    result = df.fill_nan(-1)
-    result_pd = pd.api.interchange.from_dataframe(result.dataframe)
-    result_pd = result_pd.astype("float64")
-    expected = pd.DataFrame({"a": [1.0, 2.0, -1.0]})
-    pd.testing.assert_frame_equal(result_pd, expected)
-
-
 def test_column_fill_nan(library: str) -> None:
     # todo: test with nullable pandas
     ser = nan_series_1(library)
