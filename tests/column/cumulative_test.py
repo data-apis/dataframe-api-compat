f21b85693c3268e11cbadf42bc870df1e9c695e6
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-07-30 11:44:11 +0100

    wip

---
+++ tests/column/cumulative_test.py
@@ -0,0 +1,24 @@
+import pytest
+from tests.utils import integer_series_1, convert_series_to_pandas_numpy
+import pandas as pd
+
+
+@pytest.mark.parametrize(
+    ("func", "expected_data"),
+    [
+        ("cumulative_sum", [1, 3, 6]),
+        ("cumulative_prod", [1, 2, 6]),
+        ("cumulative_max", [1, 2, 3]),
+        ("cumulative_min", [1, 1, 1]),
+    ],
+)
+def test_cumulative_functions_column(
+    library: str, func: str, expected_data: list[float]
+) -> None:
+    ser = integer_series_1(library)
+    namespace = ser.__column_namespace__()
+    expected = pd.Series(expected_data, name="result")
+    result = namespace.dataframe_from_dict({"result": getattr(ser, func)()})
+    result_pd = pd.api.interchange.from_dataframe(result.dataframe)["result"]
+    result_pd = convert_series_to_pandas_numpy(result_pd)
+    pd.testing.assert_series_equal(result_pd, expected)
--- tests/test_standard.py
+++ tests/test_standard.py
@@ -27,27 +27,6 @@ def test_fill_null_noop_column(library: str) -> None:
     assert result.column[2] != result.column[2]
 
 
-@pytest.mark.parametrize(
-    ("func", "expected_data"),
-    [
-        ("cumulative_sum", [1, 3, 6]),
-        ("cumulative_prod", [1, 2, 6]),
-        ("cumulative_max", [1, 2, 3]),
-        ("cumulative_min", [1, 1, 1]),
-    ],
-)
-def test_cumulative_functions_column(
-    library: str, func: str, expected_data: list[float]
-) -> None:
-    ser = integer_series_1(library)
-    namespace = ser.__column_namespace__()
-    expected = pd.Series(expected_data, name="result")
-    result = namespace.dataframe_from_dict({"result": getattr(ser, func)()})
-    result_pd = pd.api.interchange.from_dataframe(result.dataframe)["result"]
-    result_pd = convert_series_to_pandas_numpy(result_pd)
-    pd.testing.assert_series_equal(result_pd, expected)
-
-
 def test_column_max(library: str) -> None:
     result = integer_series_1(library).max()
     assert result == 3
