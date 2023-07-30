afdd20c8eb5a1fd96dcfb7f9b53df18a57e641ba
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-07-30 11:33:12 +0200

    rows by mask

--- tests/test_standard.py
+++ tests/test_standard.py
@@ -20,24 +20,6 @@ from tests.utils import (
 )
 
 
-def test_column_is_null_1(library: str) -> None:
-    ser = nan_series_1(library)
-    namespace = ser.__column_namespace__()
-    result = namespace.dataframe_from_dict({"result": ser.is_null()})
-    result_pd = pd.api.interchange.from_dataframe(result.dataframe)["result"]
-    expected = pd.Series([False, False, False], name="result")
-    pd.testing.assert_series_equal(result_pd, expected)
-
-
-def test_column_is_null_2(library: str, request: pytest.FixtureRequest) -> None:
-    ser = null_series_1(library, request)
-    namespace = ser.__column_namespace__()
-    result = namespace.dataframe_from_dict({"result": ser.is_null()})
-    result_pd = pd.api.interchange.from_dataframe(result.dataframe)["result"]
-    expected = pd.Series([False, False, True], name="result")
-    pd.testing.assert_series_equal(result_pd, expected)
-
-
 @pytest.mark.parametrize(
     ("ser_factory", "other_factory", "expected_values"),
     [
