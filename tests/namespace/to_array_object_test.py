8aae70c69b223ad2a22f0a9c5a8e42c364e3fba8
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-08-01 14:17:51 +0100

    add missing tests

---
+++ tests/namespace/to_array_object_test.py
@@ -0,0 +1,17 @@
+from tests.utils import integer_dataframe_1
+from tests.utils import integer_series_1
+import numpy as np
+
+
+def test_to_array_object(library: str) -> None:
+    df = integer_dataframe_1(library)
+    result = np.asarray(df.to_array_object(dtype="int64"))
+    expected = np.array([[1, 4], [2, 5], [3, 6]], dtype=np.int64)
+    np.testing.assert_array_equal(result, expected)
+
+
+def test_column_to_array_object(library: str) -> None:
+    df = integer_series_1(library)
+    result = np.asarray(df.to_array_object(dtype="int64"))
+    expected = np.array([1, 2, 3], dtype=np.int64)
+    np.testing.assert_array_equal(result, expected)
