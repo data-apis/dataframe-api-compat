3f74e8b1d3af79c9f977d6ff6cfbff88b0ef5d64
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-08-02 19:56:45 +0100

    0.1.3

--- dataframe_api_compat/__init__.py
+++ dataframe_api_compat/__init__.py
@@ -1,1 +1,6 @@
 from __future__ import annotations
+
+import dataframe_api_compat.pandas_standard
+import dataframe_api_compat.polars_standard  # noqa
+
+__all__ = ["pandas_standard", "polars_standard"]
