d21cff6fda6cf041f9c1def29bc73c9e24738443
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-08-09 23:42:48 +0100

    lazy invert test

--- tests/column/invert_test.py
+++ tests/column/invert_test.py
@@ -1,22 +1,17 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 import pandas as pd
 
-from tests.utils import bool_series_1
+from tests.utils import bool_dataframe_1
 from tests.utils import convert_series_to_pandas_numpy
 from tests.utils import interchange_to_pandas
 
-if TYPE_CHECKING:
-    import pytest
 
-
-def test_column_invert(library: str, request: pytest.FixtureRequest) -> None:
-    ser = bool_series_1(library, request)
-    namespace = ser.__column_namespace__()
-    result = namespace.dataframe_from_dict({"result": (~ser).rename("result")})
+def test_column_invert(library: str) -> None:
+    df = bool_dataframe_1(library)
+    ser = df.get_column_by_name("a")
+    result = df.insert(0, "result", ~ser)
     result_pd = interchange_to_pandas(result, library)["result"]
     result_pd = convert_series_to_pandas_numpy(result_pd)
-    expected = pd.Series([False, True, False], name="result")
+    expected = pd.Series([False, False, True], name="result")
     pd.testing.assert_series_equal(result_pd, expected)
