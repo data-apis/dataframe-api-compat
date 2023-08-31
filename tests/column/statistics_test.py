fda1ccd57fd4e54ec798af3d85389434c14f1f24
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-08-31 09:16:22 +0200

    statistics test

--- tests/column/statistics_test.py
+++ tests/column/statistics_test.py
@@ -1,24 +1,29 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import pandas as pd
 
-from tests.utils import integer_series_1
-from tests.utils import integer_series_5
-
-if TYPE_CHECKING:
-    import pytest
+from tests.utils import convert_series_to_pandas_numpy
+from tests.utils import integer_dataframe_1
+from tests.utils import interchange_to_pandas
 
 
-def test_mean(library: str, request: pytest.FixtureRequest) -> None:
-    result = integer_series_5(library, request).mean()
-    assert result == 2.0
+def test_mean(library: str) -> None:
+    df = integer_dataframe_1(library)
+    namespace = df.__dataframe_namespace__()
+    result = df.insert_column(
+        (namespace.col("a") - namespace.col("a").mean()).rename("result")
+    )
+    result_pd = interchange_to_pandas(result, library)["result"]
+    result_pd = convert_series_to_pandas_numpy(result_pd)
+    expected = pd.Series([-1, 0, 1.0], name="result")
+    pd.testing.assert_series_equal(result_pd, expected)
 
 
-def test_std(library: str, request: pytest.FixtureRequest) -> None:
-    result = integer_series_5(library, request).std()
-    assert abs(result - 1.7320508075688772) < 1e-8
+# def test_std(library: str, request: pytest.FixtureRequest) -> None:
+#     result = integer_series_5(library, request).std()
+#     assert abs(result - 1.7320508075688772) < 1e-8
 
 
-def test_column_max(library: str, request: pytest.FixtureRequest) -> None:
-    result = integer_series_1(library, request).max()
-    assert result == 3
+# def test_column_max(library: str, request: pytest.FixtureRequest) -> None:
+#     result = integer_series_1(library, request).max()
+#     assert result == 3
