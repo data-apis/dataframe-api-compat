540c0f0175ca3e56b5e8e07115df6dbb95b4fffd
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-08-31 14:52:25 +0200

    restore get_rows test

--- tests/dataframe/get_rows_test.py
+++ tests/dataframe/get_rows_test.py
@@ -8,11 +8,12 @@ from tests.utils import interchange_to_pandas
 
 
 def test_get_rows(library: str) -> None:
-    return None  # todo
     df = integer_dataframe_1(library)
     namespace = df.__dataframe_namespace__()
-    result = df.insert_column(namespace.col("a").get_rows([0, 2, 1]).rename("result"))
+    df = df.insert_column((namespace.col("a") - 1).sort(ascending=False).rename("result"))
+    namespace = df.__dataframe_namespace__()
+    result = df.get_rows(namespace.col("result"))
     result_pd = interchange_to_pandas(result, library)
-    result_pd = convert_dataframe_to_pandas_numpy(result_pd)["result"]
-    expected = pd.Series([1, 3, 2], name="result")
-    pd.testing.assert_series_equal(result_pd, expected)
+    result_pd = convert_dataframe_to_pandas_numpy(result_pd)
+    expected = pd.DataFrame({"a": [3, 2, 1], "b": [6, 5, 4], "result": [0, 1, 2]})
+    pd.testing.assert_frame_equal(result_pd, expected)
