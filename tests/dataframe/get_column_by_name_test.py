16fb1722725ebf133625cebc8f515f164d367aa8
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-09-07 14:19:32 +0200

    restore more tests

---
+++ tests/dataframe/get_column_by_name_test.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import pandas as pd
+
+from tests.utils import convert_dataframe_to_pandas_numpy
+from tests.utils import integer_dataframe_1
+from tests.utils import interchange_to_pandas
+
+
+def test_get_column_by_name(library: str) -> None:
+    df = integer_dataframe_1(library)
+    namespace = df.__dataframe_namespace__()
+    col = namespace.col
+    result = col("a").rename("_tmp")
+    result = df.insert_column(result).drop_column("a").rename_columns({"_tmp": "a"})
+    df.__dataframe_namespace__()
+    result_pd = interchange_to_pandas(result, library)
+    result_pd = convert_dataframe_to_pandas_numpy(result_pd)
+    expected = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})[["b", "a"]]
+    pd.testing.assert_frame_equal(result_pd, expected)
