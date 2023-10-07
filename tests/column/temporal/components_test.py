b660c198f4130fa88aa8728a5f74d5d529b92803
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-07 12:56:05 +0300

    test microsecond too

--- tests/column/temporal/components_test.py
+++ tests/column/temporal/components_test.py
@@ -20,11 +20,12 @@ from tests.utils import temporal_dataframe_1
 def test_expr_components(library: str, attr: str, expected: list[int]) -> None:
     df = temporal_dataframe_1(library)
     col = df.__dataframe_namespace__().col
-    result = df.select(getattr(col("a").dt, attr)())
-    result_list = result.collect().get_column_by_name("a")
-    assert result_list.get_value(0) == expected[0]
-    assert result_list.get_value(1) == expected[1]
-    assert result_list.get_value(2) == expected[2]
+    for col_name in ("a", "c", "e"):
+        result = df.select(getattr(col(col_name).dt, attr)())
+        result_list = result.collect().get_column_by_name(col_name)
+        assert result_list.get_value(0) == expected[0]
+        assert result_list.get_value(1) == expected[1]
+        assert result_list.get_value(2) == expected[2]
 
 
 @pytest.mark.parametrize(
@@ -41,7 +42,42 @@ def test_expr_components(library: str, attr: str, expected: list[int]) -> None:
 )
 def test_col_components(library: str, attr: str, expected: list[int]) -> None:
     df = temporal_dataframe_1(library).collect()
-    result = getattr(df.get_column_by_name("a").dt, attr)()
+    for col_name in ("a", "c", "e"):
+        result = getattr(df.get_column_by_name(col_name).dt, attr)()
+        assert result.get_value(0) == expected[0]
+        assert result.get_value(1) == expected[1]
+        assert result.get_value(2) == expected[2]
+
+
+@pytest.mark.parametrize(
+    ("col_name", "expected"),
+    [
+        ("a", [123000, 321000, 987000]),
+        ("c", [123543, 321654, 987321]),
+        ("e", [123543, 321654, 987321]),
+    ],
+)
+def test_expr_microsecond(library: str, col_name: str, expected: list[int]) -> None:
+    df = temporal_dataframe_1(library)
+    col = df.__dataframe_namespace__().col
+    result = df.select(col(col_name).dt.microsecond())
+    result_list = result.collect().get_column_by_name(col_name)
+    assert result_list.get_value(0) == expected[0]
+    assert result_list.get_value(1) == expected[1]
+    assert result_list.get_value(2) == expected[2]
+
+
+@pytest.mark.parametrize(
+    ("col_name", "expected"),
+    [
+        ("a", [123000, 321000, 987000]),
+        ("c", [123543, 321654, 987321]),
+        ("e", [123543, 321654, 987321]),
+    ],
+)
+def test_col_microsecond(library: str, col_name: str, expected: list[int]) -> None:
+    df = temporal_dataframe_1(library).collect()
+    result = df.get_column_by_name(col_name).dt.microsecond()
     assert result.get_value(0) == expected[0]
     assert result.get_value(1) == expected[1]
     assert result.get_value(2) == expected[2]
