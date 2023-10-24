60749246daa7cc6a5f89171da39b59bb1e035017
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-24 15:44:56 +0100

    clean up

--- tests/dataframe/insert_test.py
+++ tests/dataframe/insert_test.py
@@ -61,29 +61,3 @@ def test_insert_eager_columns(library: str, request: pytest.FixtureRequest) -> N
     df_pd = convert_dataframe_to_pandas_numpy(df_pd)
     expected = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
     pd.testing.assert_frame_equal(df_pd, expected)
-
-
-def test_insert_reduction(library: str) -> None:
-    # this is well-defined so probably should be supported?
-    pass  # todo
-    # df = integer_dataframe_1(library)
-    # new_col = (df.col("b").mean()).rename("result")
-    # result = df.assign(new_col)
-    # result_pd = interchange_to_pandas(result, library)
-    # result_pd = convert_dataframe_to_pandas_numpy(result_pd)
-    # expected = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "result": [5.0, 5.0, 5.0]})
-    # pd.testing.assert_frame_equal(result_pd, expected)
-
-
-def test_insert_reduction_and_column(library: str) -> None:
-    pass  # todo
-    # df = integer_dataframe_1(library)
-    # df.__dataframe_namespace__()
-    # col = df.col
-    # result = df.assign(col("b").mean().rename("c"), col("b").rename("d"))
-    # result_pd = interchange_to_pandas(result, library)
-    # result_pd = convert_dataframe_to_pandas_numpy(result_pd)
-    # expected = pd.DataFrame(
-    #     {"a": [1, 2, 3], "b": [4, 5, 6], "c": [5.0, 5.0, 5.0], "d": [4, 5, 6]}
-    # )
-    # pd.testing.assert_frame_equal(result_pd, expected)
