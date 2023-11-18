f3d712209ea1d5e217dfb0f6b6ae843343952254
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-11-18 07:59:32 +0000

    separate tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
+import sys
 from typing import Any
+
+LIBRARIES = {
+    (3, 8): ["polars-lazy"],
+    (3, 9): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
+    (3, 10): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
+    (3, 11): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
+    (3, 12): ["polars-lazy"],
+}
 
 
 def pytest_generate_tests(metafunc: Any) -> None:
     if "library" in metafunc.fixturenames:
         metafunc.parametrize(
             "library",
-            [
-                "pandas-numpy",
-                "pandas-nullable",
-                "polars-lazy",
-            ],
+            LIBRARIES[sys.version_info[:2]],
         )
