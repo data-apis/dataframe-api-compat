f5513778ad4f013406a353edc0c7636e9587d4cf
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-11-21 17:39:56 +0000

    fixup

--- tests/column/schema_test.py
+++ tests/column/schema_test.py
@@ -1,8 +1,15 @@
 from __future__ import annotations
 
+import pandas as pd
+import pytest
+
 from tests.utils import mixed_dataframe_1
 
 
+@pytest.mark.skipif(
+    tuple(int(v) for v in pd.__version__.split(".")) < (2, 0, 0),
+    reason="no pyarrow support",
+)
 def test_schema(library: str) -> None:
     df = mixed_dataframe_1(library)
     namespace = df.__dataframe_namespace__()
