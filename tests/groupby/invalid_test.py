b831c99e64673bbc01f462194c78ea8f7f7f90d0
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-04 10:34:59 +0000

    further cleanup

--- tests/groupby/invalid_test.py
+++ tests/groupby/invalid_test.py
@@ -1,16 +1,12 @@
 from __future__ import annotations
 
-from typing import Any
-from typing import Callable
-
 import pytest
 
 from tests.utils import integer_dataframe_1
 
 
-@pytest.mark.parametrize("maybe_collect", [lambda x: x, lambda x: x.persist()])
-def test_groupby_invalid(library: str, maybe_collect: Callable[[Any], Any]) -> None:
-    df = maybe_collect(integer_dataframe_1(library)).select("a")
+def test_groupby_invalid(library: str) -> None:
+    df = integer_dataframe_1(library).select("a")
     with pytest.raises((KeyError, TypeError)):
         df.group_by(0)
     with pytest.raises((KeyError, TypeError)):
