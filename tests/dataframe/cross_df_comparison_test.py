23165340cf080710d2e55edc924efec3ff6080f0
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-31 10:27:54 +0000

    remove xfail

--- tests/dataframe/cross_df_comparison_test.py
+++ tests/dataframe/cross_df_comparison_test.py
@@ -6,10 +6,9 @@ from tests.utils import integer_dataframe_1
 from tests.utils import integer_dataframe_2
 
 
-@pytest.mark.xfail(strict=False)
 def test_invalid_comparisons(library: str) -> None:
-    df1 = integer_dataframe_1(library)
-    df2 = integer_dataframe_2(library)
-    mask = df2.col("a") > 1
+    df1 = integer_dataframe_1(library).persist()
+    df2 = integer_dataframe_2(library).persist()
+    mask = df2.get_column("a") > 1
     with pytest.raises(ValueError):
         _ = df1.filter(mask)
