cf744a644aa3606b75181d868e4cb2438d8195a8
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-31 09:52:05 +0000

    only 9 xfails

--- tests/dataframe/any_rowwise_test.py
+++ tests/dataframe/any_rowwise_test.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import pytest
-
 from tests.utils import bool_dataframe_1
 from tests.utils import compare_dataframe_with_reference
 
@@ -13,11 +11,3 @@ def test_any_horizontal(library: str) -> None:
     result = df.filter(mask)
     expected = {"a": [True, True, False], "b": [True, True, True]}
     compare_dataframe_with_reference(result, expected, dtype=pdx.Bool)
-
-
-@pytest.mark.xfail(strict=False)
-def test_any_horizontal_invalid(library: str) -> None:
-    df = bool_dataframe_1(library)
-    pdx = df.__dataframe_namespace__()
-    with pytest.raises(ValueError):
-        _ = pdx.any_horizontal(pdx.col("a"), (df + 1).col("b"))
