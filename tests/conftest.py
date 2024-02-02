346c489f1d3191475fc62cdf8251ecb5a3a52e44
Anatoly Myachev <anatoly.myachev@intel.com>
2024-02-02 17:00:58 +0100

    add 'library' option
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,6 @@ from __future__ import annotations
 
 import sys
 from typing import Any
-import pytest
-
-LIBRARIES = {
-    (3, 8): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
-    (3, 9): ["pandas-numpy", "pandas-nullable", "polars-lazy", "modin"],
-    (3, 10): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
-    (3, 11): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
-    (3, 12): ["polars-lazy"],
-}
 
 
 class PandasHandler:
@@ -108,14 +99,57 @@ class ModinHandler:
         )
 
 
-@pytest.fixture(scope="session", params=LIBRARIES[sys.version_info[:2]])
-def library(request):
-    library_name = request.param
-    if library_name in ("pandas-numpy", "pandas-nullable"):
-        yield PandasHandler(library_name)
-    elif library_name == "polars-lazy":
-        yield PolarsHandler(library_name)
-    elif library_name == "modin":
-        yield ModinHandler(library_name)
+LIBRARIES = {
+    (3, 8): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
+    (3, 9): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
+    (3, 10): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
+    (3, 11): ["pandas-numpy", "pandas-nullable", "polars-lazy"],
+    (3, 12): ["polars-lazy"],
+}
+
+LIBRARIES_HANDLERS = {
+    "pandas-numpy": PandasHandler("pandas-numpy"),
+    "pandas-nullable": PandasHandler("pandas-nullable"),
+    "polars-lazy": PolarsHandler("polars-lazy"),
+    "modin": ModinHandler("modin"),
+}
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--library",
+        action="store",
+        default=None,
+        type=str,
+        help="library to test",
+    )
+
+
+def pytest_configure(config):
+    library = config.option.library
+    if library is None:
+        # `LIBRARIES` is already initialized
+        return
     else:
-        raise NotImplementedError
+        assert library in ("pandas-numpy", "pandas-nullable", "polars-lazy", "modin")
+        global LIBRARIES
+        LIBRARIES = {
+            (3, 8): [library],
+            (3, 9): [library],
+            (3, 10): [library],
+            (3, 11): [library],
+            (3, 12): [library],
+        }
+
+    # TODO: potential way to add filterwarnings for different librariess
+    # config.addinivalue_line(
+    #    "filterwarnings", "ignore:Ray execution environment not yet initialized:UserWarning",
+    # )
+
+
+def pytest_generate_tests(metafunc: Any) -> None:
+    if "library" in metafunc.fixturenames:
+        libraries = LIBRARIES[sys.version_info[:2]]
+        lib_handlers = [LIBRARIES_HANDLERS[lib] for lib in libraries]
+
+        metafunc.parametrize("library", lib_handlers, ids=libraries)
