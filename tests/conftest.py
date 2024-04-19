ce346d58f80c0b861664cac3d0073a308c008af3
Anatoly Myachev <anatoly.myachev@intel.com>
2024-04-20 00:26:20 +0200

    fix
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,7 +33,7 @@ def pytest_addoption(parser: Any) -> None:
 
 def pytest_configure(config: Any) -> None:
     library = config.option.library
-    if library is None:
+    if library is None:  # pragma: no cover
         # `LIBRARIES` is already initialized
         return
     else:
